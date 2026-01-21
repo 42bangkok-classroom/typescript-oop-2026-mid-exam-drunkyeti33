64e72a43fd7ad61c84ba1afebcc32b80da6e2734
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-21 14:03:44 +0700

    1

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,20 @@
 // Write your code below
+const grade = Number(process.argv[2])
+if (Number.isNaN(grade) || grade > 100 || grade < 0){
+    console.log("Invalid input")
+}
+else if(grade>=80){
+    console.log("Grade is A")
+}
+else if(grade>=70){
+    console.log("Grade is B")
+}
+else if(grade>=60){
+    console.log("Grade is C")
+}
+else if(grade>=50){
+    console.log("Grade is D")
+}
+else if(grade>=0){
+    console.log("Grade is F")
+}
