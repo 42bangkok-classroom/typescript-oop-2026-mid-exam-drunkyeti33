253f8abc7a93fdd774b9c7d377f9dea3f81fa49f
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-01-21 14:51:08 +0700

    3

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,9 @@
 // Write your code below
+const fizz = Number(process.argv[2])
+for(let i = 1; i < fizz+1; i++){
+    if(i%3===0 && i%5===0){console.log("FizzBuzz")}
+    else if(i%5===0){console.log("Buzz")}
+    else if(i%3===0){console.log("Fizz")}
+    else{
+    console.log(i)}
+}
